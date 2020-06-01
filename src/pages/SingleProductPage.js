9cea58abafd80ad365d58f08a8bf3204e396bf36
Satyendra Singh Yadav <satyendras767@gmail.com>
2020-06-01 21:06:18 +0530

    initial commit

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -17,25 +17,26 @@ export default function SingleProductPage() {
              return <h1>Product Loading....</h1>
           }
           const {company,description,id,price,title,image} = singleProduct;
-          return <section className="py-5">
-                    <div className="container">
-                    <div className="row">
+          return (
+            <section className="py-5">
+                  <div className="container">
+                   <div className="row">
                     <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
-                    <img src={`../${image}`} alt="singleProductImg" className="img-fluid" />
+                      <img src={`../${image}`} alt="singleProductImg" className="img-fluid" />
                     </div>
-                    <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
-                    <h5 className="text-title mb-4">Modle:{title}</h5>
-                    <h5 className="text-capitalize text-muted">Company: {company}</h5>
-                    <h5 className="text-main text-capitalize">Price:${price}</h5>
+                  <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
+                    <h5 className="text-title mb-4">Modle : {title}</h5>
+                    <h5 className="text-capitalize text-muted">Company : {company}</h5>
+                    <h5 className="text-main text-capitalize">Price : $ {price}</h5>
                     <p className="text-capitalize text-title mt-3">Some Info About Product :</p>
                     <p>{description}</p>
                     <button className="main-link" style={{margin:"0.75rem"}} onClick={() => addToCart(id)}>Add To Cart</button>
                     <Link to="/products" className="main-link" style={{margin:"0.75rem"}}>Back To Products</Link>
-                   
-                     </div>
-                    </div>
-                    </div>   
-                </section>
+                  </div>
+                </div>
+              </div>   
+            </section>
+          )
         }}
         </ProductConsumer> 
         </>
